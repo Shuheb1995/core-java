9b071baa32bbde71ee5e6096c39e3e5523caa7bc
Shuheb1995 <shuheb.xworkz@gmail.com>
2023-06-08 04:28:45 +0530

    HashMap-code

--- Refrigerator/src/com/xworkz/refrigerator/RefrigeratorRunner.java
+++ Refrigerator/src/com/xworkz/refrigerator/RefrigeratorRunner.java
@@ -1,10 +1,13 @@
 package com.xworkz.refrigerator;
 
 import java.util.HashMap;
+import java.util.Set;
 
 import com.xworkz.refrigerator.dao.RefrigeratorDao;
 import com.xworkz.refrigerator.dao.RefrigeratorDaoImpl;
 import com.xworkz.refrigerator.dto.RefrigeratorDto;
+import com.xworkz.refrigerator.service.RefrigeratorService;
+import com.xworkz.refrigerator.service.RefrigeratorServiceImpl;
 
 public class RefrigeratorRunner {
 
@@ -14,29 +17,34 @@ public class RefrigeratorRunner {
 		RefrigeratorDto dto1 = new RefrigeratorDto("Whirpool", "Black", 12000, "Single Door", 5);
 		RefrigeratorDto dto2 = new RefrigeratorDto("Panasonic", "Grey", 20000, "Multi", 10);
 
-		RefrigeratorDao dao = new RefrigeratorDaoImpl();
+		//RefrigeratorDao dao = new RefrigeratorDaoImpl();
+		RefrigeratorService service = new RefrigeratorServiceImpl();
 
-		dao.save(1, dto);
-		dao.save(2, dto1);
-		dao.save(3, dto2);
+		service.save(1, dto);
+		service.save(2, dto1);
+		service.save(3, dto2);
 		System.out.println("-------------------------------------------------");
 
-		HashMap<Integer, RefrigeratorDto> read = dao.read();
-		System.out.println(read);
+		HashMap<Integer, RefrigeratorDto> read = service.read();
+		Set<Integer> re = read.keySet();
+		for (Integer i : re) {
+			System.out.println(read.get(i));
+		}
+		
 
 		System.out.println("-------------------------------------------------");
 
-		RefrigeratorDto find = dao.find(1);
+		RefrigeratorDto find = service.find(1);
 		System.out.println(find);
 
 		System.out.println("-------------------------------------------------");
 
-		boolean update = dao.update(2, "Sony");
+		boolean update = service.update(2, "Sony");
 		System.out.println(update);
 
 		System.out.println("-------------------------------------------------");
 
-		boolean del = dao.delete(2);
+		boolean del = service.delete(2);
 		System.out.println(del);
 
 	}
